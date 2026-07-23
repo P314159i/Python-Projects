c80a89a418aee13e63fca25237054058345df6a7
Pi Di <pidi@f0r6s13.codam.nl>
2026-07-23 22:52:44 +0200

    x

---
+++ ft_first_exception.py
@@ -0,0 +1,25 @@
+def input_temperature(temp_str: str) -> int:
+    return int(temp_str)
+
+
+def test_temperature() -> None:
+    print("Input data is '25'")
+    temperature: int = input_temperature("25")
+    print(f"Temperature is now {temperature}°C\n")
+
+    print("Input data is 'abc'")
+    try:
+        input_temperature("abc")
+    except ValueError as err:
+        print(f"Caught input_temperature error: {err}\n")
+
+    print("All tests completed - program didn't crash!")
+
+
+def main() -> None:
+    print("=== Garden Temperature ===\n")
+    test_temperature()
+
+
+if __name__ == "__main__":
+    main()
