31d4f959f529f3cab2a3853c0339cfb230b0cc02
Pi Di <pidi@f1r4s12.codam.nl>
2026-07-25 17:56:12 +0200

    x

--- 42school/Basic Python Modules/Module p02/ex1/ft_raise_exception.py
+++ 42school/Basic Python Modules/Module p02/ex1/ft_raise_exception.py
@@ -1,31 +1,54 @@
-def input_temperature(temp_str: str) -> int:
-    if temp_str in range (0, 40):
-        return int(temp_str)
+def input_temperature(temp_str: str | int) -> int:
+    print(f"Input data is '{temp_str}'")
     try:
-        if int(temp_str) < 0:
-            raise Exception (
-                f"Caught input_temperature error: {int(temp_str)}°C is too cold for plants (min 0°C)"
-    exfep            )
-    raise Exception (
-        f"Caught input_temperature error: {int(temp_str)}°C is too hot for plants (max 40°C)"
+        temperature = int(temp_str)
+    except ValueError as err:
+        raise Exception(
+            f"Caught input_temperature error: {err}\n"
         )
 
+    if temperature in range(0, 41):
+        print(f"Temperature is now {temperature}°C\n")
+        return temperature
+
+    if temperature < 0:
+        raise Exception(
+            f"Caught input_temperature error: "
+            f"{temperature}°C is too cold for plants (min 0°C)\n"
+        )
+    else:
+        raise Exception(
+            f"Caught input_temperature error: "
+            f"{temperature}°C is too hot for plants (max 40°C)\n"
+        )
+
+
 def test_temperature() -> None:
-    print("Input data is '125'")
-    print(f"Temperature is now {int(input_temperature("125"))}°C\n")
-
-    print("Input data is 'abc'")
     try:
-        input_temperature("abc")
-    except ValueError as err:
-        print(f"Caught input_temperature error: {err}\n")
+        input_temperature('25')
+    except Exception as err:
+        print(err)
 
-    print("All tests completed - program didn't crash!")
+    try:
+        input_temperature('abc')
+    except Exception as err:
+        print(err)
+
+    try:
+        input_temperature('100')
+    except Exception as err:
+        print(err)
+
+    try:
+        input_temperature('-50')
+    except Exception as err:
+        print(err)
 
 
 def main() -> None:
-    print("=== Garden Temperature ===\n")
+    print("=== Garden Temperature Checker ===\n")
     test_temperature()
+    print("All tests completed - program didn't crash!")
 
 
 if __name__ == "__main__":
